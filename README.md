ec6f7efc6c3c19c6f66502c0a2ca6b7b9919d180
Federico Lancerin <federico.lancerin@gmail.com>
2023-08-24 17:09:31 +0200

    first push

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # pi-cam
-Here is a script that I can use to take a camera pic on my rPi and upload it to some well-known location for later use
+Here is a script that I can use to take a camera pic on my rPi and upload it to some well-known locations on S3 for later use
+
+
+## Prerequisites
+
+### fswebcam
+The CLI utility we'll use to take the pic and save it to file
+```bash
+apt install fswebcam
+```
+
+### awscli
+The CLI utility we'll use to upload the picture to S3
+```bash
+pip install awscli
+```
+
+### S3 bucket/buckets and IAM user
+This script assumes the presence of local credentials sufficient to call `aws s3 cp` and one or more destination buckets that the user has permissions to upload to.
---
+++ capture-and-upload.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This script is ran on a schedule to take a picture from the default system camera and upload it to an S3 bucket.
+
+
+DATE=$(date +"%Y-%m-%d_%H%M")
+FILENAME="${DATE}.jpg"
+
+PROJECT="chili_0"
+S3_DESTINATION_LIVE="s3://cam-fede-rocks-live/captures/${PROJECT}"
+S3_DESTINATION_LT="s3://cam-fede-rocks-long-term/${PROJECT}"
+
+
+# Take picture
+fswebcam --gmt \
+ --frames 20 \
+ --skip 10 \
+ --delay 3 \
+ --resolution "1280x720" \
+ --line-colour "#FF000000" \
+ --banner-colour "#FF000000" \
+ --text-colour "#FFFF00" \
+ --font "sans:20" \
+ "${FILENAME}"
+
+
+# Upload to S3
+echo "Uploading ${FILENAME} to ${S3_DESTINATION_LIVE}"
+aws s3 cp "${FILENAME}" "${S3_DESTINATION_LIVE}/latest.jpg"
+
+echo "Uploading ${FILENAME} to ${S3_DESTINATION_LT}"
+aws s3 cp "${FILENAME}" "${S3_DESTINATION_LT}/${FILENAME}"
+
+
+# Remove local file
+echo "Deleting local file ${FILENAME}.."
+rm "${FILENAME}"
